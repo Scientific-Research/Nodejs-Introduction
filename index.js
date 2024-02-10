68ccea120f78002b34c7db121a67437d63fdef34
Maximilian <scientificresearch309@gmail.com>
2024-02-10 23:47:42 +0100

    write on the file was done successfully using writeFileSync()

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
 
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+// console.log(textIn);
+
+const textOut = `This is what we know about the Avocado: ${textIn}.\nCreated on ${Date().toString()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File was written!");
