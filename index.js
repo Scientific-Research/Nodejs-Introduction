d0dfd048c922a71bc57936f484f4a71a79d8eab1
Maximilian <scientificresearch309@gmail.com>
2024-02-11 00:48:45 +0100

    and now, we have our final.txt which contains both data2 and data3 wrote using fs.writeFile()

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
       // we use callback here => err and data
       console.log(data3);
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("Your file has been written :)");
+      });
     });
   });
 });
