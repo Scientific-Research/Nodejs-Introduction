f0d144b0ab315fc30ced97663f9ab3380feb2e51
Maximilian <scientificresearch309@gmail.com>
2024-02-11 15:49:00 +0100

    Our server is running on PORT 8000

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const http = require("http");
+
+//////////////////////////////////
+//////FILE
 
 /* Blocking, synchronous way */
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -8,23 +12,36 @@ const fs = require("fs");
 // console.log("File was written!");
 
 /* Non-blocking, asynchronous way */
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) return console.log("ERROR! :(");
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR! :(");
 
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    // we use callback here => err and data
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-      // we use callback here => err and data
-      console.log(data3);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     // we use callback here => err and data
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       // we use callback here => err and data
+//       console.log(data3);
 
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("Your file has been written :)");
-      });
-    });
-  });
-});
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written :)");
+//       });
+//     });
+//   });
+// });
 
-console.log("Will read file");
+// console.log("Will read file");
 // As result, first of all, we see the "Will read file" sentence and then what
 // is in start.txt as data => read-this
+
+//////////////////////////////////
+//////SERVER
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server");
+});
+
+const PORT = 8000;
+/* "127.0.0.1" is our default localhost IP address on our computer*/
+server.listen(PORT, "127.0.0.1", () => {
+  console.log(`Server is runnning on PORT ${PORT}`);
+});
