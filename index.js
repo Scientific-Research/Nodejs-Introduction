e7c585c590760c640b32601a2c9a9ec99b46441d
Maximilian <scientificresearch309@gmail.com>
2024-02-12 23:52:25 +0100

    creating a separate module and export it to the index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 
 //////////////////////////////////
 //////FILE
@@ -36,21 +37,6 @@ const url = require("url");
 
 //////////////////////////////////
 //////SERVER
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%productName%}/g, product.productName);
-  output = output.replace(/{%image%}/g, product.image);
-  output = output.replace(/{%price%}/g, product.price);
-  output = output.replace(/{%from%}/g, product.from);
-  output = output.replace(/{%nutrients%}/g, product.nutrients);
-  output = output.replace(/{%quantity%}/g, product.quantity);
-  output = output.replace(/{%description%}/g, product.description);
-  output = output.replace(/{%id%}/g, product.id);
-
-  if (!product.organic) {
-    output = output.replace(/{%not_organic%}/g, "not-organic");
-  }
-  return output;
-};
 
 // this is the top level code and is started once when we ever start the program:
 // and it doesn't matter if it blocks the execution, because it is the Synchronous
